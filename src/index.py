156f136ad0b97dd1d4c167e2c55d2376bf8f4696
Matias Altamirano <mat.altamirano.22@gmail.com>
2021-01-19 12:05:10 -0300

    jasdasd

--- src/index.py
+++ src/index.py
@@ -124,7 +124,7 @@ def main():
                                 }
                             })
 
-                            #replace_line('config.py', 9, "'temp':90}")
+                            replace_line('src/config.py', 9, "'temp':90}")
 
                             
 
